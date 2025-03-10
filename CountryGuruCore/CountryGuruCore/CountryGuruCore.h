7c38ec144891690da81fae12ef4c18f68733df01
Damian Modernell <damian.modernell@gmail.com>
2025-03-10 17:10:57 -0300

    Creating macos framework that will implement all the core logic

---
+++ CountryGuruCore/CountryGuruCore/CountryGuruCore.h
@@ -0,0 +1,18 @@
+//
+//  CountryGuruCore.h
+//  CountryGuruCore
+//
+//  Created by Damian Modernell on 10/3/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CountryGuruCore.
+FOUNDATION_EXPORT double CountryGuruCoreVersionNumber;
+
+//! Project version string for CountryGuruCore.
+FOUNDATION_EXPORT const unsigned char CountryGuruCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CountryGuruCore/PublicHeader.h>
+
+
---
+++ CountryGuruCore/CountryGuruCoreTests/CountryGuruCoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  CountryGuruCoreTests.swift
+//  CountryGuruCoreTests
+//
+//  Created by Damian Modernell on 10/3/25.
+//
+
+import Testing
+@testable import CountryGuruCore
+
+struct CountryGuruCoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
