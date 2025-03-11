32e5751e22ed4f268e068abf2b6f04c7ebc857e7
Damian Modernell <damian.modernell@gmail.com>
2025-03-11 10:21:47 -0300

    test throws_on_mapping_query_response_error

--- CountryGuruCore/CountryGuruCoreTests/CountryGuruCoreTests.swift
+++ CountryGuruCore/CountryGuruCoreTests/CountryGuruCoreTests.swift
@@ -8,11 +8,19 @@
 import Testing
 @testable import CountryGuruCore
 
+struct QueryResponse {
+    
+}
+
 struct CountryCapitalQuestion {
     let countryName: String
     
     var queryPath: String {
         return "/name/\(countryName)"
+    }
+    
+    var responseMapper: (Data) throws -> QueryResponse {
+        { _ in throw anyError }
     }
 }
 
@@ -24,5 +32,19 @@ struct CountryGuruCoreTests {
         
         #expect(sut.queryPath == "/name/\(aCountry)")
     }
+    
+    @Test func throws_on_mapping_query_response_error() async throws {
+        let aCountry = "aCountry"
+        let invalidData = "invalidData".data(using: .utf8)!
+        let sut = CountryCapitalQuestion(countryName: aCountry)
 
+        #expect(throws: anyError, performing: {
+            try sut.responseMapper(invalidData)
+        })
+    }
+
+}
+
+var anyError: NSError {
+    NSError(domain: "", code: 0, userInfo: nil)
 }
