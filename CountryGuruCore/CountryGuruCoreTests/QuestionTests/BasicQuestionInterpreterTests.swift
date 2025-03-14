484e020eec2ea240262b5a8f460b32d4a36c1ae0
Damian Modernell <damian.modernell@gmail.com>
2025-03-14 16:05:19 -0300

    Implementing BasicQuestionInterpreter test throws_on_no_matching_inquiry

--- CountryGuruCore/CountryGuruCoreTests/QuestionTests/BasicQuestionInterpreterTests.swift
+++ CountryGuruCore/CountryGuruCoreTests/QuestionTests/BasicQuestionInterpreterTests.swift
@@ -6,11 +6,31 @@
 //
 
 import Testing
+@testable import CountryGuruCore
+
+struct BasicQuestionInterpreter {
+}
+
+extension BasicQuestionInterpreter: InquiryInterpreter {
+    func inquiry(from question: String) throws -> any CountryGuruCore.Inquiry {
+        throw anyError
+    }
+}
 
 struct BasicQuestionInterpreterTests {
 
-    @Test func <#test function name#>() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @Test func throws_on_no_matching_inquiry() throws {
+        let sut = makeSUT()
+        
+        #expect(throws: anyError, performing: {
+            try sut.inquiry(from: "invalid question")
+        })
     }
 
+}
+
+extension BasicQuestionInterpreterTests {
+    func makeSUT() -> BasicQuestionInterpreter {
+        BasicQuestionInterpreter()
+    }
 }
