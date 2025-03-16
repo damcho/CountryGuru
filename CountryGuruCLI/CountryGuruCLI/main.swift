f4f09482c13af7c58b26e7f0ec91ca7bac7570fb
Damian Modernell <damian.modernell@gmail.com>
2025-03-16 20:43:33 -0300

    Implementing CLI app reading from command line and printing the result

--- CountryGuruCLI/CountryGuruCLI/main.swift
+++ CountryGuruCLI/CountryGuruCLI/main.swift
@@ -6,6 +6,21 @@
 //
 
 import Foundation
+import CountryGuruCore
 
-print("Hello, World!")
+let questionHandler = CountryGuruComposer.compose()
+
+while let question = readLine() {
+    Task {
+        do {
+            let reply = try await questionHandler.didAskRaw(question)
+            print(reply.responseString)
+        } catch {
+            print("There was an issue, please try again")
+        }
+    }
+}
+
+RunLoop.current.run()
+
 
