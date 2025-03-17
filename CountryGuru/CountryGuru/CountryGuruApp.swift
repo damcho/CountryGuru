d9991efc67ea194abaf38c6cffd38f7c1a31641e
Damian Modernell <damian.modernell@gmail.com>
2025-03-17 17:01:41 -0300

    Creating iOS CountryGuru app

---
+++ CountryGuru/CountryGuru/CountryGuruApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryGuruApp.swift
+//  CountryGuru
+//
+//  Created by Damian Modernell on 17/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CountryGuruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
