a07a1d88985cb9eb13dc475fc6e8e270010feb24
Damian Modernell <damian.modernell@gmail.com>
2025-03-17 19:30:59 -0300

    Creating viewmodifier to align sender message to leading alignment

---
+++ CountryGuru/CountryGuru/Views/SenderMessageView.swift
@@ -0,0 +1,23 @@
+//
+//  SenderMessageView.swift
+//  CountryGuru
+//
+//  Created by Damian Modernell on 17/3/25.
+//
+
+import SwiftUI
+
+struct SenderMessageView: ViewModifier {
+    func body(content: Content) -> some View {
+        HStack {
+            content.frame(maxWidth: 200, alignment: .leading)
+            Spacer()
+        }.padding()
+    }
+}
+
+extension View {
+    func senderMessageSide() -> some View {
+        modifier(SenderMessageView())
+    }
+}
