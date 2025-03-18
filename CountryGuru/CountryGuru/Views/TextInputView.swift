a34ee92666dd7ff5066db79a381b0157304337f1
Damian Modernell <damian.modernell@gmail.com>
2025-03-17 21:18:55 -0300

    TextInputViewModel initial implementation

--- CountryGuru/CountryGuru/Views/TextInputView.swift
+++ CountryGuru/CountryGuru/Views/TextInputView.swift
@@ -7,23 +7,32 @@
 
 import SwiftUI
 
+class TextInputViewModel {
+    func didTapSend(with message: String) {
+        
+    }
+}
+
 struct TextInputView: View {
+    let viewModel: TextInputViewModel
+    @State private var message: String = ""
+    
     var body: some View {
         HStack {
             TextField(
-                "User name",
-                text: .constant("")
+                "",
+                text: $message
             )
             .textFieldStyle(.roundedBorder)
             .textInputAutocapitalization(.never)
             
             Button("Send") {
-                
-            }
+                viewModel.didTapSend(with: message)
+            }.disabled(message.isEmpty)
         }.padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
     }
 }
 
 #Preview {
-    TextInputView()
+    TextInputView(viewModel: TextInputViewModel())
 }
