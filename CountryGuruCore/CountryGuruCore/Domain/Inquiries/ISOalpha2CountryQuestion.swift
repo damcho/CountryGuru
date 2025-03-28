73da3992a5993d33987cd54ef405ebc467d6052e
Damian Modernell <damian.modernell@gmail.com>
2025-03-28 15:39:51 -0300

    moving code to production

---
+++ CountryGuruCore/CountryGuruCore/Domain/Inquiries/ISOalpha2CountryQuestion.swift
@@ -0,0 +1,44 @@
+//
+//  ISOalpha2CountryQuestion.swift
+//  CountryGuruCore
+//
+//  Created by Damian Modernell on 28/3/25.
+//
+
+import Foundation
+
+struct DecodableAlpha2Code: Decodable {
+    let cca2: String
+}
+
+struct ISOalpha2CountryQuestion: Inquiry {
+    let countryName: String
+    func mappedResponse(from data: Data, httpURLResponse: HTTPURLResponse) throws -> QueryResponse {
+        guard httpURLResponse.statusCode == 200 else {
+            throw HTTPClientError.notFound
+        }
+        let capitalCityArray = try JSONDecoder().decode(
+            [DecodableAlpha2Code].self,
+            from: data
+        )
+        guard let cca2 = capitalCityArray.first?.cca2 else {
+            throw QueryResponseError.decoding
+        }
+        
+        return .text(cca2)
+    }
+    
+    var queryItems: [URLQueryItem] {
+        [URLQueryItem(name: "fields", value: "cca2")]
+    }
+    
+    var queryPath: String {
+        return "/name/\(countryName)"
+    }
+    
+    func makeURL(from baseURL: URL) -> URL {
+        baseURL
+            .appendingPathComponent(queryPath)
+            .appending(queryItems: queryItems)
+    }
+}
--- CountryGuruCore/CountryGuruCoreTests/QuestionTests/ISOalphaCountryQuestionTests.swift
+++ CountryGuruCore/CountryGuruCoreTests/QuestionTests/ISOalphaCountryQuestionTests.swift
@@ -8,42 +8,6 @@
 import Testing
 @testable import CountryGuruCore
 
-struct DecodableAlpha2Code: Decodable {
-    let cca2: String
-}
-
-struct ISOalpha2CountryQuestion: Inquiry {
-    let countryName: String
-    func mappedResponse(from data: Data, httpURLResponse: HTTPURLResponse) throws -> QueryResponse {
-        guard httpURLResponse.statusCode == 200 else {
-            throw HTTPClientError.notFound
-        }
-        let capitalCityArray = try JSONDecoder().decode(
-            [DecodableAlpha2Code].self,
-            from: data
-        )
-        guard let cca2 = capitalCityArray.first?.cca2 else {
-            throw QueryResponseError.decoding
-        }
-        
-        return .text(cca2)
-    }
-    
-    var queryItems: [URLQueryItem] {
-        [URLQueryItem(name: "fields", value: "cca2")]
-    }
-    
-    var queryPath: String {
-        return "/name/\(countryName)"
-    }
-    
-    func makeURL(from baseURL: URL) -> URL {
-        baseURL
-            .appendingPathComponent(queryPath)
-            .appending(queryItems: queryItems)
-    }
-}
-
 struct ISOalphaCountryQuestionTests: InquirySpecs {
     @Test func query_path() async throws {
         let aCountry = "aCountry"
