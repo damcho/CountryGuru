9d9111b9bcd7a08578892ecd8754cc1383856bd5
Damian Modernell <damian.modernell@gmail.com>
2025-07-25 13:17:36 -0300

    tests refactor

--- CountryGuruCore/CountryGuruCoreTests/QuestionTests/BasicQuestionInterpreterTests.swift
+++ CountryGuruCore/CountryGuruCoreTests/QuestionTests/BasicQuestionInterpreterTests.swift
@@ -29,45 +29,30 @@ struct BasicQuestionInterpreterTests {
 
     @Test
     func maps_country_capital_question_successfully() throws {
-        let countryName = "argentina"
-        let inquiries = [
-            AnyInquiry(
-                question: CountryCapitalQuestion.question,
-                inquiryCreator: { countryQuestion in
-                    CountryCapitalQuestion(countryName: countryQuestion) as Inquiry
-                }
-            )
-        ]
-
-        let sut = makeSUT(with: inquiries)
-        let question = try sut.inquiry(from: "What is the capital of \(countryName)")
-
-        #expect(question is CountryCapitalQuestion)
-        #expect((question as? CountryCapitalQuestion)?.countryName == countryName)
-    }
-
-    @Test
-    func maps_large_country_capital_question_successfully() throws {
-        let countryName = "united states"
-        let inquiries = [
-            AnyInquiry(
-                question: CountryCapitalQuestion.question,
-                inquiryCreator: { countryQuestion in
-                    CountryCapitalQuestion(countryName: countryQuestion) as Inquiry
-                }
-            )
-        ]
-
-        let sut = makeSUT(with: inquiries)
-        let question = try sut.inquiry(from: "What is the capital of \(countryName)")
-
-        #expect(question is CountryCapitalQuestion)
-        #expect((question as? CountryCapitalQuestion)?.countryName == countryName)
+        try expect("argentina")
+        try expect("united states")
     }
 }
 
 extension BasicQuestionInterpreterTests {
     func makeSUT(with inquiriesMap: [AnyInquiry]) -> BasicQuestionInterpreter {
         BasicQuestionInterpreter(supportedInquiries: inquiriesMap)
+    }
+
+    private func expect(_ countryName: String) throws {
+        let inquiries = [
+            AnyInquiry(
+                question: CountryCapitalQuestion.question,
+                inquiryCreator: { countryQuestion in
+                    CountryCapitalQuestion(countryName: countryQuestion) as Inquiry
+                }
+            )
+        ]
+
+        let sut = makeSUT(with: inquiries)
+        let question = try sut.inquiry(from: "What is the capital of \(countryName)")
+
+        #expect(question is CountryCapitalQuestion)
+        #expect((question as? CountryCapitalQuestion)?.countryName == countryName)
     }
 }
