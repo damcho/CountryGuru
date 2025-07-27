3ae940ce8ace09f06110d57f61238a8c5969a2d8
Damian Modernell <damian.modernell@gmail.com>
2025-07-26 22:46:04 -0300

    capturing inquiry task so we can cancel it when a new inquiry is performed to prevent the screen to be scrolled multiple times when the response callback is invojked

--- CountryGuru/CountryGuru/ViewModel/InquiryChatScreenViewModel.swift
+++ CountryGuru/CountryGuru/ViewModel/InquiryChatScreenViewModel.swift
@@ -15,18 +15,21 @@ typealias InquiryResolverFactory = () -> InquiryResponseViewModel
 class InquiryChatScreenViewModel: ObservableObject {
     @Published var rows: [IdentifiableChatRow] = []
     let inquiryResolverFactory: InquiryResolverFactory
+    var inquityTask: Task<Void, Never>? = nil
 
     init(inquiryResolverFactory: @escaping InquiryResolverFactory) {
         self.inquiryResolverFactory = inquiryResolverFactory
     }
 
     func ask(question: String, onInquiryResponse: @escaping () -> Void) {
+        inquityTask?.cancel()
         let inquiryResolver = inquiryResolverFactory()
         rows.append(IdentifiableChatRow(message: question))
         rows.append(IdentifiableChatRow(message: inquiryResolver))
 
-        Task.detached {
+        inquityTask = Task.detached {
             await inquiryResolver.ask(question)
+            if Task.isCancelled { return }
             await MainActor.run {
                 onInquiryResponse()
             }
--- CountryGuru/CountryGuruTests/InquiryChatScreenViewModelTests.swift
+++ CountryGuru/CountryGuruTests/InquiryChatScreenViewModelTests.swift
@@ -18,6 +18,34 @@ struct InquiryChatScreenViewModelTests {
 
         #expect(await sut.rows.count == 2)
     }
+
+    @Test
+    func cancells_inquiry_response_callback_on_second_inquiry() async throws {
+        var firstInquiryResponseCallbackCalled = false
+        var secondInquiryResponseCallbackCalled = false
+
+        let sut = await makeSUT()
+
+        await confirmation("wait for response callback to be called", expectedCount: 1) { Confirmation in
+            await sut.ask(question: "first inquiry") {
+                firstInquiryResponseCallbackCalled = true
+                Confirmation()
+            }
+            let task1 = await sut.inquityTask
+            await sut.ask(question: "second inquiry") {
+                secondInquiryResponseCallbackCalled = true
+                Confirmation()
+            }
+            let task2 = await sut.inquityTask
+            await sut.inquityTask?.value
+
+            #expect(task1?.isCancelled == true)
+            #expect(task2?.isCancelled == false)
+        }
+
+        #expect(firstInquiryResponseCallbackCalled == false)
+        #expect(secondInquiryResponseCallbackCalled == true)
+    }
 }
 
 extension InquiryChatScreenViewModelTests {
